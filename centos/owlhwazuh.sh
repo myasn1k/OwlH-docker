2417221798ccc770c76b27b55545b1f303c230cc
Francisco Javier Ahijado <fjahijado@gmail.com>
2019-10-15 03:30:10 +0200

    CentOS OwlH Node container

---
+++ centos/owlhwazuh.sh
@@ -0,0 +1,48 @@
+MANAGER_IP="MANAGER_IP"
+#source env.conf
+
+rpm --import http://packages.wazuh.com/key/GPG-KEY-WAZUH
+cat > /etc/yum.repos.d/wazuh.repo <<\EOF
+[wazuh_repo]
+gpgcheck=1
+gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
+enabled=1
+name=Wazuh repository
+baseurl=https://packages.wazuh.com/3.x/yum/
+protect=1
+EOF
+yum -y install wazuh-agent
+
+if [ ! -d "/var/ossec" ]; then
+    echo -e "\e[91mThere was a problem installing Wazuh-Agent, /var/ossec path doesn't exist\e[0m"
+    exit 1
+fi
+
+sed -i "s/^enabled=1/enabled=0/" /etc/yum.repos.d/wazuh.repo
+sed -i "s/MANAGER_IP/$MANAGER_IP/" /var/ossec/etc/ossec.conf
+
+cat >> /var/ossec/etc/ossec.conf <<\EOF
+<ossec_config>
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/var/log/suricata/eve.json</location>
+  </localfile>
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/usr/local/zeek/logs/current/conn.log</location>
+  </localfile>
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/usr/local/zeek/logs/current/dns.log</location>
+  </localfile>
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/usr/local/zeek/logs/current/http.log</location>
+  </localfile>
+  <localfile>
+    <log_format>syslog</log_format>
+    <location>/usr/local/zeek/logs/current/ssl.log</location>
+  </localfile>
+</ossec_config>
+EOF
+
---
+++ centos/owlhzeek.sh
@@ -0,0 +1,53 @@
+# Zeek
+
+INTERFACE=eth0 
+#source env.conf
+
+# Main packages 
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install wget git
+yum -y install jq openssl-devel PyYAML lz4-devel gcc libpcap-devel pcre-devel libyaml-devel file-devel zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel tar make libnetfilter_queue-devel lua-devel cmake make gcc-c++ flex bison python-devel swig 
+
+# ZEEK
+cd /tmp
+git clone --recursive https://github.com/zeek/zeek
+
+cd zeek
+./configure
+make 
+make install
+
+if [ ! -d "/usr/local/zeek" ]; then
+    echo -e "\e[91mThere was a problem installing Zeek, /usr/local/zeek path doesn't exist\e[0m"
+    exit 1
+fi
+
+
+sed -i "s/eth0/$INTERFACE/g" /usr/local/zeek/etc/node.cfg
+
+cat >> /usr/local/zeek/share/zeek/site/local.zeek <<\EOF
+@load policy/tuning/json-logs.zeek
+@load owlh.zeek
+EOF
+
+cat >> /usr/local/zeek/share/zeek/site/owlh.zeek <<\EOF
+redef record DNS::Info += {
+    bro_engine:    string    &default="DNS"    &log;
+};
+redef record Conn::Info += {
+    bro_engine:    string    &default="CONN"    &log;
+};
+redef record Weird::Info += {
+    bro_engine:    string    &default="WEIRD"    &log;
+};
+redef record SSL::Info += {
+    bro_engine:    string    &default="SSL"    &log;
+};
+redef record SSH::Info += {
+    bro_engine:    string    &default="SSH"    &log;
+};
+EOF
+
+/usr/local/zeek/bin/zeekctl deploy
+
+#(sudo crontab -l ; echo "*/5 * * * * /usr/local/zeek/bin/zeekctl cron ") | crontab -
